0921c014fdf860701b92b83a9a3cae29018de6de
pine <2277367263@qq.com>
2023-05-16 09:01:10 +0800

    test(Buddy): 添加伙伴系统测试程序

---
+++ Buddy_test.c
@@ -0,0 +1,15 @@
+#include "Buddy.h"
+#include <stdio.h>
+
+int main()
+{
+    buddy_system buddy_sys;
+    buddy_system_init(&buddy_sys, 31, 1000);
+    buddy_system_print(&buddy_sys);
+
+    buddy_system_push(&buddy_sys, 992, 5);
+    buddy_system_print(&buddy_sys);
+
+    buddy_system_close(&buddy_sys);
+    puts("====\nDone\n====");
+}
--- Makefile
+++ Makefile
@@ -3,3 +3,7 @@ CC = gcc
 Page_test: Page_test.c VPage.c PPage.c
 	@$(CC) $^ -o $@.bin
 	@./$@.bin
+
+Buddy_test: Buddy_test.c Buddy.c VPage.c PPage.c
+	@$(CC) $^ -o $@.bin
+	@./$@.bin
