d61cd3edf72e2d7824dddad68de2074c9e04b83b
pine <2277367263@qq.com>
2023-05-16 14:05:13 +0800

    test(buddy): 使用非2次幂空间进行测试

--- buddy_test.c
+++ buddy_test.c
@@ -3,11 +3,15 @@
 
 int main()
 {
+    int res;
     buddy_system buddy_sys;
     buddy_system_init(&buddy_sys, 31, 1000);
     buddy_system_print(&buddy_sys);
 
-    buddy_system_push(&buddy_sys, 992, 5);
+    buddy_system_free(&buddy_sys, 1000, 24);
+    buddy_system_print(&buddy_sys);
+
+    buddy_system_alloc(&buddy_sys, 24, &res);
     buddy_system_print(&buddy_sys);
 
     buddy_system_close(&buddy_sys);
